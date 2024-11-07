65ae18ef51d790b6c914b73e3708e2371ddec0ce
Andrii Petryk <apetryk.work@gmail.com>
2024-11-07 13:36:46 -0500

    Refactor task management to use generic repository
    Replaced custom TaskRepository with a generic TypeORM repository for Task entity. Updated TasksService, TasksController, and module imports accordingly to maintain functionality. Simplified code by removing unnecessary repository abstractions.

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,44 @@
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async createUser(authCredentiasDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentiasDto;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.usersRepository.create({
+      username,
+      password: hashedPassword,
+    });
+    try {
+      await this.usersRepository.save(user);
+    } catch (error) {
+      console.log(error);
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.createUser(authCredentialsDto);
+  }
+}
